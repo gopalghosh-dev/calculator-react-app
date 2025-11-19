a7692ea9fcbe2e1389f7ff8732eb5677a71c461b
Gopal Ghosh <gopalghosh.dev@gmail.com>
2025-11-19 19:26:23 +0530

    Button,Card,Input add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,12 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Nextjs Starter Frontend",
-	description: "Production grade Next.js starter template",
+	title: "Basic | Calculator App",
+	description: "Basic Calculator App",
 };
 
 const page = () => {
-	return (
-		<section className="grid h-[90dvh] place-items-center">
-			<div className="space-y-2 text-center">
-				<h1 className="text-5xl font-semibold">Nextjs Starter Frontend</h1>
-				<h2 className="text-3xl">Production grade Next.js starter template</h2>
-			</div>
-		</section>
-	);
+	return <section className="grid h-[90dvh] place-items-center"></section>;
 };
 
 export default page;
